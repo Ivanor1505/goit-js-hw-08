d4785a7d33b4ee0e48ba01787dd4b0f3c8a310b3
Ivanor1505 <ivanor1505@gmail.com>
2023-06-28 17:56:59 +0300

    HW 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+const savedState = JSON.parse(localStorage.getItem(localStorageKey));
+
+if (savedState) {
+  emailInput.value = savedState.email || '';
+  messageInput.value = savedState.message || '';
+}
+
+const saveFormState = throttle(function() {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(state));
+}, 500);
+
+form.addEventListener('input', saveFormState);
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  console.log('Form values:', savedState);
+
+  localStorage.removeItem(localStorageKey);
+  emailInput.value = '';
+  messageInput.value = '';
+});
